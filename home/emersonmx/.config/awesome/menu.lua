213002985f22e272385a880c4b436bb8fe13f1f4
Emerson MX <emersonmx@gmail.com>
2013-12-27 05:52:18 -0300

    Adicionando várias configurações do awesome.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ home/emersonmx/.config/awesome/menu.lua
@@ -0,0 +1,19 @@
+-- Create a laucher widget and a main menu
+myawesomemenu = {
+    { "manual", terminal .. " -x man awesome" },
+    { "edit config", editor_cmd .. " " .. awesome.conffile },
+    { "restart", awesome.restart },
+    { "quit", awesome.quit }
+}
+
+mymainmenu = awful.menu({
+    items = {
+        { "awesome", myawesomemenu, beautiful.awesome_icon },
+        { "open terminal", terminal }
+    }
+})
+
+mylauncher = awful.widget.launcher({
+    image = image(beautiful.awesome_icon),
+    menu = mymainmenu
+})
