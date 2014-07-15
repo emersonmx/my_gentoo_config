b26f0834418251715b60bfb6138ff494a08ba5ab
Emerson MX <emersonmx@gmail.com>
2014-07-15 13:05:25 -0300

    Removendo configurações do slim e adicionando modelo para a criação do Xresources usando o comando "xrdb -merge Xresource -edit .Xresources".
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- home/emersonmx/.config/devilspie2/terminal.lua
+++ home/emersonmx/.config/devilspie2/terminal.lua
@@ -1,3 +1,8 @@
-if get_window_name() == "Xfce4-terminal" then
+if get_window_name() == "Terminal" then
+    maximize()
+end
+
+if get_window_name() == "xterm" then
+    undecorate_window()
     maximize()
 end
