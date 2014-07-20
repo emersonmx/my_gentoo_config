645fda34a0431bcd0c5017cdb2f0d01bbe98ab8c
Emerson MX <emersonmx@gmail.com>
2014-07-20 12:16:28 -0300

    Alterando o script para deixar ele mais genérico.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- check_diff.sh
+++ check_diff.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-use_editor=echo
+use_editor=
 
-if test $# -eq 1
-then
-    use_editor=$1
+if [[ $# == 2 ]]; then
+    use_editor=echo
+elif [[ $# == 3 ]]; then
+    use_editor=$3
 else
-    echo "Uso: $0 <diff_editor>"
-    echo Imprimindo apenas o nome dos arquivos.
+    echo "Uso: $0 <src> <dst> [<diff_editor>]"
+    exit 0
 fi
 
 IFS="
 "
 
-for e in `diff -q -r . / | grep -v Somente`
+for e in `diff -q -r $1 $2 | grep -v Somente`
 do
     file_a=`echo $e | awk '{print $3}'`
     file_b=`echo $e | awk '{print $5}'`
-    $use_editor -f $file_a $file_b
+    $use_editor $file_a $file_b
 done
