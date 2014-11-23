d25ba5530a5f401824eb1ca475896ae37122cedf
Emerson MX <emersonmx@gmail.com>
2014-11-23 16:11:40 -0300

    Reduzindo as mensagens de erro.

--- home/emersonmx/.pythonrc.py
+++ home/emersonmx/.pythonrc.py
@@ -4,10 +4,9 @@ try:
 except ImportError:
     # Fallback to the stdlib readline completer if it is installed.
     # Taken from http://docs.python.org/2/library/rlcompleter.html
-    print("Jedi is not installed, falling back to readline")
     try:
         import readline
         import rlcompleter
         readline.parse_and_bind("tab: complete")
     except ImportError:
-        print("Readline is not installed either. No tab completion is enabled.")
+        print("No tab completion is enabled.")
