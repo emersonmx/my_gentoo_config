4959d5811094326b89db04e5b1f6bcd87c4efde4
Emerson MX <emersonmx@gmail.com>
2014-11-26 21:05:52 -0300

    Configuração do python e configurações de pacotes.

--- home/emersonmx/.pythonrc.py
+++ home/emersonmx/.pythonrc.py
@@ -10,3 +10,59 @@ except ImportError:
         readline.parse_and_bind("tab: complete")
     except ImportError:
         print("No tab completion is enabled.")
+
+import sys
+import pprint
+
+def pp_displayhook(value):
+    if value is not None:
+        try:
+            import __builtin__
+            __builtin__._ = value
+        except ImportError:
+            __builtins__._value
+
+        pprint.pprint(value)
+
+sys.displayhook = pp_displayhook
+
+import os
+import sys
+
+from code import InteractiveConsole
+from tempfile import mkstemp
+
+EDITOR = os.environ.get("EDITOR", "vi")
+EDIT_CMD = "\e"
+
+class EditableBufferInteractiveConsole(InteractiveConsole):
+
+    def __init__(self, *args, **kwargs):
+        self.last_buffer = []
+        InteractiveConsole.__init__(self, *args, **kwargs)
+
+    def runsource(self, source, *args):
+        self.last_buffer = [source.encode("utf-8")]
+        return InteractiveConsole.runsource(self, source, *args)
+
+    def raw_input(self, source, *args):
+        line = InteractiveConsole.raw_input(self, source, *args)
+        if line == EDIT_CMD:
+            fd, tmpfl = mkstemp(".py")
+            os.write(fd, b'\n'.join(self.last_buffer))
+            os.close(fd)
+            os.system("{} {}".format(EDITOR, tmpfl))
+            with open(tmpfl) as f:
+                line = f.read()
+            os.unlink(tmpfl)
+            tmpfl = ""
+            lines = line.split('\n')
+            for l in lines: self.push(l)
+            line = lines[-1]
+
+        return line
+
+c = EditableBufferInteractiveConsole(locals=locals())
+c.interact(banner="")
+
+sys.exit()
